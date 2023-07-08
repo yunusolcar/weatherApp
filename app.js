23970b061766623a778e3652c19d6e7181879408
Yunus <yunusolcar@hotmail.com>
2023-07-08 13:07:51 +0300

    main file

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+var https = require('follow-redirects').https;
+var fs = require('fs');
+
+const app = express()
+
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middleware
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+  const apiKey = '72b4b12d20b1cce11fdd6f9e521241cb';
+  const city = 'frankfurt';
+  const options = {
+    'method': 'GET',
+    'hostname': 'api.openweathermap.org',
+    'path': `https://api.openweathermap.org/data/2.5/weather?q=${city}&{}&appid=${apiKey}&lang=tr&units=metric`,
+    'headers': {},
+    'maxRedirects': 20
+  };
+
+  var req = https.request(options, function (apiRes) {
+    var chunks = [];
+
+    apiRes.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    apiRes.on("end", function () {
+      var body = Buffer.concat(chunks);
+      res.render('index', {
+        weatherData: JSON.parse(body.toString())
+      });
+    });
+
+    apiRes.on("error", function (error) {
+      console.error(error);
+    });
+  });
+
+  req.end();
+})
+
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
