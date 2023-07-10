40f2bf6e961ab5985774d85184c389cae20b3e2c
Yunus <yunusolcar@hotmail.com>
2023-07-10 10:36:17 +0300

    process.env.Port updated

--- app.js
+++ app.js
@@ -45,5 +45,5 @@ app.get('/', (req, res) => {
   req.end();
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
